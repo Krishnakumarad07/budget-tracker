f5f54c9992d2231d1cebbca35bd0fa921dacc43c
krishnakumar A D <krishnakumarad7@gmail.com>
2025-12-10 17:59:21 +0530

    Update app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,12 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api(append: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            // optionally throttle etc.
-        ]);
-        //
+   ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(HandleCors::class); // ✅ Use native middleware
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
