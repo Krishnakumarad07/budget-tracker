d799e1930db4f4be28ed17ac2d5504b876f0e917
krishnakumar A D <krishnakumarad7@gmail.com>
2025-12-10 17:31:54 +0530

    Update Kernel.php

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -4,13 +4,13 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use Illuminate\Routing\Middleware\SubstituteBindings;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+// use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
 class Kernel extends HttpKernel
 {
     protected $middlewareGroups = [
         'api' => [
-            EnsureFrontendRequestsAreStateful::class, // Required for SPA auth
+            // EnsureFrontendRequestsAreStateful::class, // Required for SPA auth
             'throttle:api',
             SubstituteBindings::class,
         ],
